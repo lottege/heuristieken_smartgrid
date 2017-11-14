676525f7f800dc40803ce6c27e8e99186b23c65e
lottege <siEwu6xy>
2017-11-14 11:58:37 +0100

    nu met battery_nr

--- cable_list.py
+++ cable_list.py
@@ -8,9 +8,10 @@ import experimentimportTXT
 
 
 class Cable:
-    def __init__(self, pos_x, pos_y):
+    def __init__(self, pos_x, pos_y, battery_nr):
         self.pos_x = pos_x
         self.pos_y = pos_y
+        self.battery_nr = battery_nr
 
 
 cable_list = []
@@ -24,27 +25,27 @@ for i in range(len(batteries)):
     for j in range(len(houses)):
         if experimentimportTXT.match_with_house(houses[j], batteries[i]) and houses[j].output > 0:
 
-            cable = Cable(houses[j].pos_x, houses[j].pos_y)
+            cable = Cable(houses[j].pos_x, houses[j].pos_y, i)
             cable_list.append(cable)
 
             # cable loops through x
             while cable_list[-1].pos_x != batteries[i].pos_x:
                 if cable_list[-1].pos_x < batteries[i].pos_x:
-                    cable = Cable(cable_list[-1].pos_x + 1, cable_list[-1].pos_y)
+                    cable = Cable(cable_list[-1].pos_x + 1, cable_list[-1].pos_y, i)
                     cable_list.append(cable)
 
                 elif cable_list[-1].pos_x > batteries[i].pos_x:
-                    cable = Cable(cable_list[-1].pos_x - 1, cable_list[-1].pos_y)
+                    cable = Cable(cable_list[-1].pos_x - 1, cable_list[-1].pos_y, i)
                     cable_list.append(cable)
 
             # cable loops through y
             while cable_list[-1].pos_y != batteries[i].pos_y:
                 if cable_list[-1].pos_y < batteries[i].pos_y:
-                    cable = Cable(cable_list[-1].pos_x, cable_list[-1].pos_y + 1)
+                    cable = Cable(cable_list[-1].pos_x, cable_list[-1].pos_y + 1, i)
                     cable_list.append(cable)
 
                 elif cable_list[-1].pos_y > batteries[i].pos_y:
-                    cable = Cable(cable_list[-1].pos_x, cable_list[-1].pos_y - 1)
+                    cable = Cable(cable_list[-1].pos_x, cable_list[-1].pos_y - 1, i)
                     cable_list.append(cable)
 
             if cable_list[-1].pos_y == batteries[i].pos_y and cable_list[-1].pos_x == batteries[i].pos_x:
