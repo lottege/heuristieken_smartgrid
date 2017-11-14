d87d1c8536e26e9b7d58a3e104c0644855e4a77d
lottege <siEwu6xy>
2017-11-14 15:57:36 +0100

    de huizen worden optimaal met elkaar verbonden

--- experimentimportTXT.py
+++ experimentimportTXT.py
@@ -26,6 +26,13 @@ class House:
         House.number += 1
 
 
+class Cable:
+    def __init__(self, pos_x, pos_y, battery_nr):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.battery_nr = battery_nr
+
+
 def readtxt(txtfile):
     with open(txtfile, newline='') as batterijen1:
         readCSV = csv.reader(batterijen1, delimiter=']')
@@ -59,3 +66,47 @@ def match_with_house(house, battery):
     else:
         return False
 
+
+def connect_to_battery(house, battery, cable_list, batteries, houses):
+    # cable loops through x
+    connected = 0
+    while cable_list[-1].pos_x != batteries[battery].pos_x:
+        if cable_list[-1].pos_x < batteries[battery].pos_x:
+            cable = Cable(cable_list[-1].pos_x + 1, cable_list[-1].pos_y, battery)
+            cable_list.append(cable)
+
+        elif cable_list[-1].pos_x > batteries[battery].pos_x:
+            cable = Cable(cable_list[-1].pos_x - 1, cable_list[-1].pos_y, battery)
+            cable_list.append(cable)
+
+    # cable loops through y
+    while cable_list[-1].pos_y != batteries[battery].pos_y:
+        if cable_list[-1].pos_y < batteries[battery].pos_y:
+            cable = Cable(cable_list[-1].pos_x, cable_list[-1].pos_y + 1, battery)
+            cable_list.append(cable)
+
+        elif cable_list[-1].pos_y > batteries[battery].pos_y:
+            cable = Cable(cable_list[-1].pos_x, cable_list[-1].pos_y - 1, battery)
+            cable_list.append(cable)
+
+    if cable_list[-1].pos_y == batteries[battery].pos_y and cable_list[-1].pos_x == batteries[battery].pos_x:
+        connected += 1
+        batteries[battery].capacity -= houses[house].output
+        houses[house].output -= houses[house].output
+
+    return cable_list
+
+
+def second_smallest(lst):
+    first = second = float("inf")
+    for num in lst:
+        if num < first:
+            second, first = first, num
+        elif first < num < second:
+            second = num
+    return second
+
+
+def third_smallest(lst):
+    lst.sort()
+    return lst[2]
