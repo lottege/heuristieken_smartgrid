f6afb9e570c75e43413f3a8be074682c76b8071e
lottege <siEwu6xy>
2017-11-14 11:22:58 +0100

    nu doet hij het volgens mij

--- cable_list.py
+++ cable_list.py
@@ -51,10 +51,13 @@ for i in range(len(batteries)):
                 connected += 1
                 batteries[i].capacity -= houses[j].output
                 houses[j].output -= houses[j].output
+                print(i, j, end="oui, ")
+                print("")
         else:
 
             # locates battery and house that returns false on MWH or has output of 0
             print(i, j, end="x, ")
+
             #debug
 
 for i in range(len(batteries)):
@@ -63,41 +66,3 @@ for i in range(len(batteries)):
 
 print(len(cable_list))
 print(connected)
-
-# # Option 2: connects cables to other cables (shorter dict, more dict-checks)
-# for battery in batteries:
-#     for house in houses:
-#         if match_with_house(house, battery):
-#
-#         Cable.pos_x = house.pos_x
-#         Cable.pos_y = house.pos_y
-#
-#         # cable loops through x
-#         for Cable.pos_x != Battery.pos_x:
-#             if Cable.pos_x < Battery.pos_x:
-#                 Cable.pos_x += 1
-#                 if cable_dict[Cable.pos_x, Cable.pos_y] == True:
-#                     break
-#                 else:
-#                     cable_dict[Cable.pos_x, Cable.pos_y] = True
-#
-#             elif Cable.pos_x > Battery.pos_x:
-#                 Cable.pos_x -= 1
-#                 if cable_dict[Cable.pos_x, Cable.pos_y] == True:
-#                     break
-#                 else:
-#                     cable_dict[Cable.pos_x, Cable.pos_y] = True
-#
-#         # cable loops through y
-#         for Cable.pos_y != Battery.pos_y:
-#             if Cable.pos_y < Battery.pos_y:
-#                 Cable.pos_y += 1
-#                 if cable_dict[Cable.pos_x, Cable.pos_y] == true:
-#                     break
-#                 cable_dict[Cable.pos_x, Cable.pos_y] = true
-#
-#             elif Cable.pos_y > Battery.pos_y:
-#                 Cable.pos_y -= 1
-#                 if cable_dict[Cable.pos_x, Cable.pos_y] == true:
-#                     break
-#                 cable_dict[Cable.pos_x, Cable.pos_y] = true
