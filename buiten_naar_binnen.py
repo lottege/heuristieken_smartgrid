09049a38b5217580add7b380fb89da2387353aad
lottege <siEwu6xy>
2017-11-21 13:42:27 +0100

    eerst wordt het huis verbonden dat het verste weg staat, dan de volgende, dan de volgende

---
+++ buiten_naar_binnen.py
@@ -0,0 +1,27 @@
+'''
+ / connects houses to the closest battery if it has enough capacity
+'''
+import experimentimportTXT
+
+
+cable_list = []
+cl = []
+connected = 0
+
+batteries = experimentimportTXT.readtxt("wijk1_batterijen.txt")
+houses = experimentimportTXT.readcsv("wijk1_huizen.csv")
+
+distance = experimentimportTXT.distance_sort(batteries, houses)
+sorted_houses = experimentimportTXT.sort_houses(houses)
+print(sorted_houses)
+for house in sorted_houses:
+    for key in distance[house[0]]:
+        if experimentimportTXT.match_with_house(houses[house[0]], batteries[key[0]]) and houses[house[0]].output > 0:
+            cable = experimentimportTXT.Cable(houses[house[0]].pos_x, houses[house[0]].pos_y, house[0])
+            cable_list.append(cable)
+            cl = experimentimportTXT.connect_to_battery(house[0], key[0], cable_list, batteries, houses)
+            connected += 1
+            break
+
+print(len(cl))
+print(connected)
--- experimentimportTXT.py
+++ experimentimportTXT.py
@@ -124,3 +124,14 @@ def distance_sort(batteries, houses):
         distances.append(sorted_distance)
 
     return distances
+
+
+def sort_houses(houses):
+    distance = {}
+    for i in range(len(houses)):
+        dis = abs((houses[i].pos_x - 25) + (houses[i].pos_y - 25))
+        distance[i] = dis
+
+    sorted_distance = sorted(distance.items(), key=operator.itemgetter(1), reverse=True)
+
+    return sorted_distance
