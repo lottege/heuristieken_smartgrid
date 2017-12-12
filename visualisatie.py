e4157a1fa99b83e7522540597c2a176159f1282d
lottege <siEwu6xy>
2017-12-12 14:07:37 +0100

    random plekken 9 power stars

--- visualisatie.py
+++ visualisatie.py
@@ -17,6 +17,14 @@ def visualisation(houses, batteries, cables):
             plt.plot(cables[i].pos_x, cables[i].pos_y, color = 'm', marker = '.', alpha=0.5)
         elif cables[i].battery_nr is 4:
             plt.plot(cables[i].pos_x, cables[i].pos_y, color = 'y', marker = '.', alpha=0.5)
+        elif cables[i].battery_nr is 5:
+            plt.plot(cables[i].pos_x, cables[i].pos_y, color = '#808000', marker = '.', alpha=0.5)
+        elif cables[i].battery_nr is 6:
+            plt.plot(cables[i].pos_x, cables[i].pos_y, color = '#F08080', marker = '.', alpha=0.5)
+        elif cables[i].battery_nr is 7:
+            plt.plot(cables[i].pos_x, cables[i].pos_y, color = '#008080', marker = '.', alpha=0.5)
+        elif cables[i].battery_nr is 8:
+            plt.plot(cables[i].pos_x, cables[i].pos_y, color = '#800080', marker = '.', alpha=0.5)
 
     for e in range(len(houses)):
         plt.plot(houses[e].pos_x, houses[e].pos_y, color='k', marker='p')
@@ -32,4 +40,12 @@ def visualisation(houses, batteries, cables):
             plt.plot(batteries[a].pos_x, batteries[a].pos_y, color='m', marker='s')
         elif a is 4:
             plt.plot(batteries[a].pos_x, batteries[a].pos_y, color='y', marker='s')
+        elif a is 5:
+            plt.plot(batteries[a].pos_x, batteries[a].pos_y, color='#808000', marker='s')
+        elif a is 6:
+            plt.plot(batteries[a].pos_x, batteries[a].pos_y, color='#F08080', marker='s')
+        elif a is 7:
+            plt.plot(batteries[a].pos_x, batteries[a].pos_y, color='#008080', marker='s')
+        elif a is 8:
+            plt.plot(batteries[a].pos_x, batteries[a].pos_y, color='#800080', marker='s')
     plt.show()
